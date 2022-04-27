1a7355d53eaab923ef255189601bdaa1642b8c88
EmilyKatie <51035738+EmilyKatie@users.noreply.github.com>
2022-04-27 11:58:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const kelvin = 293;
+//The variable is a constant because that temp never changes.
+const kelvin = 0;
+//Declaring celsius in comparison to kelvin
+const celsius = kelvin - 273;
+console.log(celsius);
+const fahrenheit = celsius*(9/5) + 32;
+console.log(fahrenheit);
+//declaring and logging fahrenheit formula in comparison to celsuis
+
+//Rounding random temp to whole number
+console.log(Math.floor(fahrenheit));
+
+var TEMPERATURE ='fahrenheit';
+//Interpolatin the variable into the string. Booyah!
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit `);
+
