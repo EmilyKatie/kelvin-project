4327af61f1d8e4d3ce1cd98040a8bead805511d7
EmilyKatie <51035738+EmilyKatie@users.noreply.github.com>
2022-04-29 13:12:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ var TEMPERATURE ='fahrenheit';
 //Interpolatin the variable into the string. Booyah!
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit `);
 
+var myAge = 21;
+//cuz i'm old and i just had a bday yesterday. 
+//Happy birthday to me!! oh and assigned a variable
+var earlyYears = 2;
+//assigned the number of years to 2 to the variable earlyYears
+console.log(earlyYears * 10.5);
+console.log(myAge - 2);
+var laterYears = myAge - 2;
+//assigning laterYears variable to 2 years minus my current age
+console.log(laterYears * 4);
+//multiplied laterYears variable by 4
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+//combined 2 variables into 1 to become dog years
+var myName = 'Emily';
+myName.toLowerCase();
+//lowercased myName variable
+console.log(myName.toLowerCase);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
